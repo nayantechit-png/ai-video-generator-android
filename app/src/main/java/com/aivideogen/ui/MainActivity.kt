f55188ec9ca87f7635d0383127226ae3a8887094
nayantechit-png <nayantech.it@gmail.com>
2026-06-14 15:20:04 +0200

    Add app/src/main/java/com/aivideogen/ui/MainActivity.kt

---
+++ app/src/main/java/com/aivideogen/ui/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.aivideogen.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.aivideogen.R
+import com.aivideogen.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfig = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.galleryFragment, R.id.settingsFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        // Hide bottom nav on generation screen
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.generateFragment -> {
+                    binding.bottomNavigation.visibility = android.view.View.GONE
+                }
+                else -> {
+                    binding.bottomNavigation.visibility = android.view.View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
